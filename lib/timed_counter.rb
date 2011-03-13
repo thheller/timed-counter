4a2f4902d5439fe381d11a4104171ec4b4194338
Thomas Heller <info@zilence.net>
2011-03-13 09:22:12 +0100

    api rewrite
    only need one TimedCounter instance
    allow for arrow keys (joined by "/")
    convert timestamps to UTC

--- lib/timed_counter.rb
+++ lib/timed_counter.rb
@@ -3,15 +3,26 @@ require 'redis'
 require 'nest'
 
 class TimedCounter
-  def initialize(key, redis)
+  def initialize(redis)
     @redis = redis
-    @key = key
-    @root = Nest.new("$tc", redis)[key]
+    @root = Nest.new("$tc", redis)
+  end
+
+  def make_key(parts)
+    if parts.is_a?(Array)
+      parts.join("/")
+    else
+      parts.to_s
+    end 
+  end
+
+  def make_ts(time)
+    (time || Time.now).utc.strftime("%Y%m%d%H%M")
   end
 
   # redis key to total will be "c:#{key}:total"
-  def incr(amount = 1, time = nil)
-    ts = (time || Time.now).strftime("%Y%m%d%H%M")
+  def incr(key, amount = 1, time = nil)
+    ts = make_ts(time)
 
     hour_key = ts[0, 10]
     min = ts[10, 2]
@@ -33,15 +44,18 @@ class TimedCounter
     # 2011-03: { 01: 0, 02: 0, ...} days
     # 2011-03-11: { 00: 0, 01: 0, 02: 0, ...} hours
     # 2011-03-11 11: { 00, 01, 02, ...} minutes
+    #
+    ckey = make_key(key)
 
     pipeline = @redis.pipelined do
       @redis.multi do
-        @root[:total].incrby(amount)
-        @root[:years].hincrby(year_key, amount)
-        @root[year_key].hincrby(month, amount)
-        @root[month_key].hincrby(day, amount)
-        @root[day_key].hincrby(hour, amount)
-        @root[hour_key].hincrby(min, amount)
+        node = @root[ckey]
+        node[:total].incrby(amount)
+        node[:years].hincrby(year_key, amount)
+        node[year_key].hincrby(month, amount)
+        node[month_key].hincrby(day, amount)
+        node[day_key].hincrby(hour, amount)
+        node[hour_key].hincrby(min, amount)
       end
     end
 
@@ -49,7 +63,7 @@ class TimedCounter
 
     # if the total count equals our amount we created this key, so keep a list of active counters
     if result[0] == amount
-      @redis.sadd("$tc_list", @key)
+      @redis.sadd("$tc_list", ckey)
     end
 
     # for now just keep everything, time will tell how much data we are talking about
@@ -91,65 +105,74 @@ class TimedCounter
     true
   end
 
+  def reset!(key)
+    @redis.keys("#{@root[make_key(key)]}*").each do |c|
+      @redis.del(c)
+    end
+  end
+
 
   # start timestamp
   # count number of steps
   # step_size number of seconds to increment steps
   # ts_index index in 201103111200
-  def query_hash_range(start, count, step_size, ts_index, ts_size = 2)
+  def query_hash_range(key, start, count, step_size, ts_index, ts_size = 2)
     hash = Hash.new { |h, k| h[k] = [] }
     count.times do |it|
-      ts = (start + (it * step_size)).strftime("%Y%m%d%H%M")
+      ts = make_ts(start + (it * step_size))
       hash[ts[0, ts_index]] << ts[ts_index, ts_size]
     end
-    
+
+    ckey = make_key(key)
+    node = @root[ckey]
+
     keys = hash.to_a.sort_by { |it| it[0] }
     mres = @redis.pipelined do
       keys.each do |key, values|
-        @root[key].hmget(*values)
+        node[key].hmget(*values)
       end
     end
 
     mres.flatten.collect(&:to_i)
   end
 
-  def total
-    @root[:total].get.to_i
+  def total(key)
+    @root[make_key(key)][:total].get.to_i
   end
 
-  def year(time = nil)
-    @root[:years].hget((time || Time.now).strftime('%Y')).to_i
+  def year(key, time = nil)
+    @root[make_key(key)][:years].hget(make_ts(time)[0, 4]).to_i
   end
 
-  def month(time = nil)
-    ts = (time || Time.now).strftime("%Y%m")
-    @root[ts[0, 4]].hget(ts[4, 2]).to_i
+  def month(key, time = nil)
+    ts = make_ts(time)
+    @root[make_key(key)][ts[0, 4]].hget(ts[4, 2]).to_i
   end
 
-  def day(time = nil)
-    ts = (time || Time.now).strftime("%Y%m%d")
-    @root[ts[0, 6]].hget(ts[6, 2]).to_i
+  def day(key, time = nil)
+    ts = make_ts(time)
+    @root[make_key(key)][ts[0, 6]].hget(ts[6, 2]).to_i
   end
 
-  def days(start, count)
-    query_hash_range(start, count, 60*60*24, 6, 2)
+  def days(key, start, count)
+    query_hash_range(key, start, count, 60*60*24, 6, 2)
   end
 
-  def hour(time = nil)
-    ts = (time || Time.now).strftime("%Y%m%d%H")
-    @root[ts[0, 8]].hget(ts[8, 2]).to_i
+  def hour(key, time = nil)
+    ts = make_ts(time)
+    @root[make_key(key)][ts[0, 8]].hget(ts[8, 2]).to_i
   end
 
-  def hours(start, count)
-    query_hash_range(start, count, 60*60, 8, 2)
+  def hours(key, start, count)
+    query_hash_range(key, start, count, 60*60, 8, 2)
   end
 
-  def minute(time = nil)
-    ts = (time || Time.now).strftime("%Y%m%d%H%M")
-    @root[ts[0, 10]].hget(ts[10, 2]).to_i
+  def minute(key, time = nil)
+    ts = make_ts(time)
+    @root[make_key(key)][ts[0, 10]].hget(ts[10, 2]).to_i
   end
 
-  def minutes(start, count)
-    query_hash_range(start, count, 60, 10, 2)
+  def minutes(key, start, count)
+    query_hash_range(key, start, count, 60, 10, 2)
   end
 end
--- spec/timed_counter_spec.rb
+++ spec/timed_counter_spec.rb
@@ -4,47 +4,50 @@ describe TimedCounter do
 
   before(:each) do 
     @redis = Redis.connect
-    @redis.flushdb
 
-    @counter = TimedCounter.new("test", @redis)
+    @counter = TimedCounter.new(@redis)
+    @counter.reset!(:test)
+  end
+
+  it "should join array keys" do
+    @counter.make_key(:test).should == "test"
+    @counter.make_key([:user, 1]).should == "user/1"
+    @counter.make_key([:user, 1, :clicks]).should == "user/1/clicks"
   end
 
   # could fail based on timing but im lazy
   it "should increment multiple values in a single call" do
-    @counter.total.should == 0
+    @counter.total(:test).should == 0
 
-    @counter.year.should == 0
-    @counter.month.should == 0
-    @counter.day.should == 0
-    @counter.hour.should == 0
-    @counter.minute.should == 0
+    @counter.year(:test).should == 0
+    @counter.month(:test).should == 0
+    @counter.day(:test).should == 0
+    @counter.hour(:test).should == 0
+    @counter.minute(:test).should == 0
 
-    @counter.incr(1)
-    @counter.total.should == 1 
+    @counter.incr(:test, 1)
+    @counter.total(:test).should == 1
+    @counter.year(:test).should == 1
+    @counter.month(:test).should == 1
+    @counter.day(:test).should == 1
+    @counter.hour(:test).should == 1
+    @counter.minute(:test).should == 1
 
-    @counter.year.should == 1
-    @counter.month.should == 1
-    @counter.day.should == 1
-    @counter.hour.should == 1
-    @counter.minute.should == 1
+    @counter.incr(:test, 100)
+    @counter.total(:test).should == 101
+    @counter.year(:test).should == 101
+    @counter.month(:test).should == 101
+    @counter.day(:test).should == 101
+    @counter.hour(:test).should == 101
+    @counter.minute(:test).should == 101
 
-    @counter.incr(100)
-    @counter.total.should == 101 
-
-    @counter.year.should == 101
-    @counter.month.should == 101
-    @counter.day.should == 101
-    @counter.hour.should == 101
-    @counter.minute.should == 101
-
-    @counter.incr(-50)
-    @counter.total.should == 51 
-
-    @counter.year.should == 51
-    @counter.month.should == 51
-    @counter.day.should == 51
-    @counter.hour.should == 51
-    @counter.minute.should == 51
+    @counter.incr(:test, -50)
+    @counter.total(:test).should == 51
+    @counter.year(:test).should == 51
+    @counter.month(:test).should == 51
+    @counter.day(:test).should == 51
+    @counter.hour(:test).should == 51
+    @counter.minute(:test).should == 51
   end
   
   it "should be queryable by minutes" do
@@ -52,11 +55,11 @@ describe TimedCounter do
     five_ago = now - 300
     ten_ago = now - 600
 
-    @counter.incr(1, five_ago)
-    @counter.incr(1, ten_ago)
+    @counter.incr(:test, 1, five_ago)
+    @counter.incr(:test, 1, ten_ago)
 
     # from 10 mins ago fetch the counts for 10 minutes
-    minutes = @counter.minutes(ten_ago, 10)
+    minutes = @counter.minutes(:test, ten_ago, 10)
     minutes.length.should == 10
     minutes.inject(0) { |x, i| x += i }.should == 2 # sum is from rails
     minutes[0].should == 1
@@ -68,11 +71,11 @@ describe TimedCounter do
     five_ago = now - (300 * 60)
     ten_ago = now - (600 * 60)
 
-    @counter.incr(1, five_ago)
-    @counter.incr(1, ten_ago)
+    @counter.incr(:test, 1, five_ago)
+    @counter.incr(:test, 1, ten_ago)
 
     # from 10 hours ago fetch the counts for 10 hours
-    minutes = @counter.hours(ten_ago, 10)
+    minutes = @counter.hours(:test, ten_ago, 10)
     minutes.length.should == 10
     minutes.inject(0) { |x, i| x += i }.should == 2
     minutes[0].should == 1
@@ -84,11 +87,11 @@ describe TimedCounter do
     five_ago = now - ((300 * 60) * 24)
     ten_ago = now - ((600 * 60) * 24)
 
-    @counter.incr(1, five_ago)
-    @counter.incr(1, ten_ago)
+    @counter.incr(:test, 1, five_ago)
+    @counter.incr(:test, 1, ten_ago)
 
     # from 10 hours ago fetch the counts for 10 hours
-    minutes = @counter.days(ten_ago, 10)
+    minutes = @counter.days(:test, ten_ago, 10)
     minutes.length.should == 10
     minutes.inject(0) { |x, i| x += i }.should == 2
     minutes[0].should == 1
